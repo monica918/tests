78c0441412b8f2a3345e0cc0e844cd3e880ae266
monica <3111355263@qq.com>
2023-08-21 14:00:14 +0800

    2023。8。21

---
+++ test.py
@@ -0,0 +1,39 @@
+print('aaa')
+{
+"OrderInfo":{
+"EndTime":"2023-08-14 16:20:32",
+"MeterValueStart":10,
+"PayChannel":6,
+"OperatorID":"313744932",
+"PaymentAmount":0,
+"ChargeDetails":[
+{
+"DetailStartTime":"2023-08-14 16:19:00",
+"DetailEndTime":"2023-08-14 16:20:00",
+"ElecPrice":7.0,
+"SevicePrice":0.8,
+"DetailPower":0.10,
+"DetailElecMoney":0.70,
+"DetailSeviceMoney":0.08
+}
+],
+"StationID":"33222207",
+"StartChargeSeq":"MA6CC2LK7202308141619149763",
+"Money":0.78,
+"Elect":0.1,
+"StopReason":0,
+"ChargeLast":66,
+"UserChargeType":3,
+"EquipmentOwnerID":"M01234567",
+"SumPeriod":1,
+"EquipmentID":"11000000000000034588286",
+"DiscountInfo":"无",
+"StartTime":"2023-08-14 16:19:26",
+"ServiceMoney":0.08,
+"MeterValueEnd":10.1,
+"MobileNumber":"",
+"ElectMoney":0.70,
+"PayTime":"2023-08-14 16:20:32",
+"Power":80.0,
+"ConnectorID":"11000000000000034588286000"
+}
